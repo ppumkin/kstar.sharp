05dc7a0b5ab584f318f267d02c3505c8663e5a0c
ppumkin <piotr@kula.solutions>
2017-01-11 14:27:25 +0000

    Upgraded the MVC4 solution into new folder Updated all packages and config files Compatible with latest Visual Studio now (testes VS2017)

---
+++ kastar.sharp.mvc5/Scripts/Inverter/InverterLatestData.js
@@ -0,0 +1,23 @@
+﻿
+
+
+$(document).ready(function () {
+    setInterval(function () { getLatest(); }, 30000); //need to synch this time setting to something common
+    getLatest();
+});
+
+function getLatest() {
+    $.ajax({
+        url: "/Home/GetLatest",
+        context: document.body
+    }).done(function (data) {
+
+        //debugger;
+        var textData = new Date(parseInt(data.RecordedDateTime.substr(6))).toString() + '     PV: ' + data.PVPower + 'kW     Bat1: ' + data.Bat1Charge + '%  ' + data.Bat1Power + 'W     Grid: ' + data.GridPower + 'W     Load: ' + data.LoadPower + 'W     EToday: ' + data.EToday;
+        $('#liveData').text(textData);
+
+        // ["Bat1Amp", "Bat1Charge", "Bat1Power", "Bat1Voltage", "EToday", "ETotal", "GridPower", "LoadPower", "PV1Volt", "PV2Volt", "PVPower", "RecordedDateTime", "TempCelcius" ]
+
+    });
+
+}
