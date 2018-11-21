956c0d0daff66c3fea8a5c8efc2b5be3b20d06a2
Piotr Kula <piotr@kula.solutions>
2018-11-21 10:28:41 +0000

    dashboard updated a bit and bound to live data

--- kstar.sharp.aspnetcore/wwwroot/js/Inverter/InverterLatestData.js
+++ kstar.sharp.aspnetcore/wwwroot/js/Inverter/InverterLatestData.js
@@ -13,10 +13,11 @@ function getLatest() {
     }).done(function (data) {
 
         //debugger;
-        var textData = new Date(parseInt(data.RecordedDateTime.substr(6))).toString() + '     PV: ' + data.PVPower + 'kW     Bat1: ' + data.Bat1Charge + '%  ' + data.Bat1Power + 'W     Grid: ' + data.GridPower + 'W     Load: ' + data.LoadPower + 'W     EToday: ' + data.EToday;
-        $('#liveData').text(textData);
+        //var textData = new Date(parseInt(data.RecordedDateTime.substr(6))).toString() + '     PV: ' + data.PVPower + 'kW     Bat1: ' + data.Bat1Charge + '%  ' + data.Bat1Power + 'W     Grid: ' + data.GridPower + 'W     Load: ' + data.LoadPower + 'W     EToday: ' + data.EToday;
+        //$('#liveData').text(textData);
 
         // ["Bat1Amp", "Bat1Charge", "Bat1Power", "Bat1Voltage", "EToday", "ETotal", "GridPower", "LoadPower", "PV1Volt", "PV2Volt", "PVPower", "RecordedDateTime", "TempCelcius" ]
+        $(document).trigger("onGetLatest", [data]);
 
     });
 
